f550fa29d9b88a6f376507eb1fb3089e9e1ca07c
Samuel <samuelchigo55@gmail.com>
2025-02-23 23:59:23 +0100

    little changes

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,8 +1,20 @@
+import styles from '@/styles'
 import React from 'react'
+import { motion } from "framer-motion";
+import {
+  navVariants,
+  slideIn,
+  staggerContainer,
+  textVariant,
+} from "@/utils/motion";
+
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className={`${styles.paddings} relative x-10`}>
+      <div className='gradient-02 x-0'/>
+    </section>
   )
 }
 
